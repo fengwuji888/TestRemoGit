0141dba0a8e1bb4c1b71ea65befb040ff636232c
fengwuji <1325482232@qq.com>
2019-03-11 13:58:57 +0800

    第二个开发者新添加了Person类

---
+++ RecordVoice/RecordVoice/Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  RecordVoice
+//
+//  Created by huatu on 2019/3/11.
+//  Copyright © 2019 huatu. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
